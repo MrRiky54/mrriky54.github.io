62f7ae6c9ea79e7addbaeb1df42e2213ac82dbf7
Riccardo Carissimi <riky0504@gmail.com>
2017-11-06 20:37:34 +0100

    Website Gone to Maintenance Mode

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <title>MrRiky54's Site</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css" type='text/css'>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Website Under Construction</h1>
+      <p>Please stand by and remember: hope is always the last to die.</p>
+      <p>But don't wait so long, probably it won't ever be released.</p>
+      <p>Meanwhile, watch this fantastic EU Flag</p>
+      <p>(Spoiler Alert: Click on The Flag)</p>
+      <a href="https://www.youtube.com/watch?v=V2QrARLZe-o" target="_blank"><img src="https://europa.eu/european-union/sites/europaeu/files/docs/body/flag_yellow_low.jpg" /></a>
+    </div>
+  </body>
+</html>
